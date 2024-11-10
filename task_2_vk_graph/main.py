2e18fe5f84042295cca2f8de35cdb0c3f6df8ab2
ewasince <ewasince@gmail.com>
2024-11-10 16:47:16 +0300

    [+] better main

--- task_2_vk_graph/main.py
+++ task_2_vk_graph/main.py
@@ -1,22 +1,37 @@
+import pickle
+from typing import Any
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 import asyncio
-import sys
 
-from graph import get_graph
+from graph import get_graph, draw_graph
 from vk_api import collect_data
 from task_2_vk_graph.analyzer import show_analyze
 import file_service
 
-async def main():
+def _pickle_res(obj: Any, filename: str):
+    with open(filename, "wb") as f:
+        pickle.dump(obj, f)
+
+async def collect_dataset():
     await collect_data()
-    # graph = await get_graph(source=)
-    # show_analyze(graph=graph)
+
+async def process_dataset():
+    data = file_service.load_data_list()
+    graph = get_graph(data)
+    analyze_dump = show_analyze(graph=graph)
+    # checkpoint
+    _pickle_res(analyze_dump, "analyze_dump")
+    draw_graph(graph, *analyze_dump)
+
+
+async def main():
+    # await collect_dataset()
+    await process_dataset()
 
 
 if __name__ == '__main__':
-    # asyncio.run(main())
-    data = file_service.load_data_list()
-    print(len(data))
+    asyncio.run(main())
